925ca6dfd5831a9f435497410b161b507d89aba8
shakhmatov <shakhmatov@selectel.ru>
2016-05-26 17:30:24 +0300

    fixed events issues

--- events.go
+++ events.go
@@ -1,6 +1,7 @@
 package libvirt
 
 import (
+	"fmt"
 	"sync"
 	"unsafe"
 )
@@ -154,16 +155,6 @@ func domainEventLifecycleCallback(c C.virConnectPtr, d C.virDomainPtr,
 	return callCallback(opaque, &connection, &domain, eventDetails)
 }
 
-//export domainEventGenericCallback
-func domainEventGenericCallback(c C.virConnectPtr, d C.virDomainPtr,
-	opaque int) int {
-
-	domain := VirDomain{ptr: d}
-	connection := VirConnection{ptr: c}
-
-	return callCallback(opaque, &connection, &domain, nil)
-}
-
 //export domainEventRTCChangeCallback
 func domainEventRTCChangeCallback(c C.virConnectPtr, d C.virDomainPtr,
 	utcoffset int64, opaque int) int {
@@ -194,14 +185,14 @@ func domainEventWatchdogCallback(c C.virConnectPtr, d C.virDomainPtr,
 
 //export domainEventIOErrorCallback
 func domainEventIOErrorCallback(c C.virConnectPtr, d C.virDomainPtr,
-	srcPath string, devAlias string, action int, opaque int) int {
+	srcPath *C.char, devAlias *C.char, action int, opaque int) int {
 
 	domain := VirDomain{ptr: d}
 	connection := VirConnection{ptr: c}
 
 	eventDetails := DomainIOErrorEvent{
-		SrcPath:  srcPath,
-		DevAlias: devAlias,
+		SrcPath:  C.GoString(srcPath),
+		DevAlias: C.GoString(devAlias),
 		Action:   action,
 	}
 
@@ -213,7 +204,7 @@ func domainEventGraphicsCallback(c C.virConnectPtr, d C.virDomainPtr,
 	phase int,
 	local C.virDomainEventGraphicsAddressPtr,
 	remote C.virDomainEventGraphicsAddressPtr,
-	authScheme string,
+	authScheme *C.char,
 	subject C.virDomainEventGraphicsSubjectPtr,
 	opaque int) int {
 
@@ -244,7 +235,7 @@ func domainEventGraphicsCallback(c C.virConnectPtr, d C.virDomainPtr,
 			Node:    C.GoString(remote.node),
 			Service: C.GoString(remote.service),
 		},
-		AuthScheme: authScheme,
+		AuthScheme: C.GoString(authScheme),
 		Subject:    subjectGo,
 	}
 
@@ -253,7 +244,7 @@ func domainEventGraphicsCallback(c C.virConnectPtr, d C.virDomainPtr,
 
 //export domainEventIOErrorReasonCallback
 func domainEventIOErrorReasonCallback(c C.virConnectPtr, d C.virDomainPtr,
-	srcPath string, devAlias string, action int, reason string,
+	srcPath *C.char, devAlias *C.char, action int, reason *C.char,
 	opaque int) int {
 
 	domain := VirDomain{ptr: d}
@@ -261,11 +252,11 @@ func domainEventIOErrorReasonCallback(c C.virConnectPtr, d C.virDomainPtr,
 
 	eventDetails := DomainIOErrorReasonEvent{
 		DomainIOErrorEvent: DomainIOErrorEvent{
-			SrcPath:  srcPath,
-			DevAlias: devAlias,
+			SrcPath:  C.GoString(srcPath),
+			DevAlias: C.GoString(devAlias),
 			Action:   action,
 		},
-		Reason: reason,
+		Reason: C.GoString(reason),
 	}
 
 	return callCallback(opaque, &connection, &domain, eventDetails)
@@ -273,13 +264,13 @@ func domainEventIOErrorReasonCallback(c C.virConnectPtr, d C.virDomainPtr,
 
 //export domainEventBlockJobCallback
 func domainEventBlockJobCallback(c C.virConnectPtr, d C.virDomainPtr,
-	disk string, _type int, status int, opaque int) int {
+	disk *C.char, _type int, status int, opaque int) int {
 
 	domain := VirDomain{ptr: d}
 	connection := VirConnection{ptr: c}
 
 	eventDetails := DomainBlockJobEvent{
-		Disk:   disk,
+		Disk:   C.GoString(disk),
 		Type:   _type,
 		Status: status,
 	}
@@ -289,16 +280,16 @@ func domainEventBlockJobCallback(c C.virConnectPtr, d C.virDomainPtr,
 
 //export domainEventDiskChangeCallback
 func domainEventDiskChangeCallback(c C.virConnectPtr, d C.virDomainPtr,
-	oldSrcPath string, newSrcPath string, devAlias string,
+	oldSrcPath *C.char, newSrcPath *C.char, devAlias *C.char,
 	reason int, opaque int) int {
 
 	domain := VirDomain{ptr: d}
 	connection := VirConnection{ptr: c}
 
 	eventDetails := DomainDiskChangeEvent{
-		OldSrcPath: oldSrcPath,
-		NewSrcPath: newSrcPath,
-		DevAlias:   devAlias,
+		OldSrcPath: C.GoString(oldSrcPath),
+		NewSrcPath: C.GoString(newSrcPath),
+		DevAlias:   C.GoString(devAlias),
 		Reason:     reason,
 	}
 
@@ -307,13 +298,13 @@ func domainEventDiskChangeCallback(c C.virConnectPtr, d C.virDomainPtr,
 
 //export domainEventTrayChangeCallback
 func domainEventTrayChangeCallback(c C.virConnectPtr, d C.virDomainPtr,
-	devAlias string, reason int, opaque int) int {
+	devAlias *C.char, reason int, opaque int) int {
 
 	domain := VirDomain{ptr: d}
 	connection := VirConnection{ptr: c}
 
 	eventDetails := DomainTrayChangeEvent{
-		DevAlias: devAlias,
+		DevAlias: C.GoString(devAlias),
 		Reason:   reason,
 	}
 
@@ -350,13 +341,13 @@ func domainEventBalloonChangeCallback(c C.virConnectPtr, d C.virDomainPtr,
 
 //export domainEventDeviceRemovedCallback
 func domainEventDeviceRemovedCallback(c C.virConnectPtr, d C.virDomainPtr,
-	devAlias string, opaque int) int {
+	devAlias *C.char, opaque int) int {
 
 	domain := VirDomain{ptr: d}
 	connection := VirConnection{ptr: c}
 
 	eventDetails := DomainDeviceRemovedEvent{
-		DevAlias: devAlias,
+		DevAlias: C.GoString(devAlias),
 	}
 	return callCallback(opaque, &connection, &domain, eventDetails)
 }
@@ -373,86 +364,122 @@ var goCallbackLock sync.RWMutex
 var goCallbacks = make(map[int]*domainCallbackContext)
 var nextGoCallbackId int
 
-//export freeCallbackId
-func freeCallbackId(goCallbackId int) {
-	goCallbackLock.Lock()
-	delete(goCallbacks, goCallbackId)
-	goCallbackLock.Unlock()
+type storageDomainCallbackContext struct {
+	sync.RWMutex
+	Data  map[int]*domainCallbackContext
+	Index int
 }
 
-func callCallback(goCallbackId int, c *VirConnection, d *VirDomain,
-	event interface{}) int {
-	goCallbackLock.RLock()
-	ctx := goCallbacks[goCallbackId]
-	goCallbackLock.RUnlock()
+var stDomCbCtx = storageDomainCallbackContext{
+	Data:  make(map[int]*domainCallbackContext),
+	Index: 0,
+}
+
+func (s *storageDomainCallbackContext) Find(key int) *domainCallbackContext {
+	s.RLock()
+	defer s.RUnlock()
+
+	return s.Data[key]
+}
+
+func (s *storageDomainCallbackContext) Erase(key int) {
+	s.Lock()
+	defer s.Unlock()
+
+	delete(s.Data, key)
+
+	return
+}
+
+func (s *storageDomainCallbackContext) Insert(value *domainCallbackContext) int {
+	s.Lock()
+	defer s.Unlock()
+
+	s.Index++
+	s.Data[s.Index] = value
+
+	return s.Index
+}
+
+func (s *storageDomainCallbackContext) Size() int {
+
+	return len(s.Data)
+}
+
+//export freeCallbackId
+func freeCallbackId(ID int) {
+	stDomCbCtx.Erase(ID)
+
+	return
+}
+
+func callCallback(ID int, c *VirConnection, d *VirDomain, event interface{}) int {
+	ctx := stDomCbCtx.Find(ID)
 	if ctx == nil {
 		// If this happens there must be a bug in libvirt
 		panic("Callback arrived after freeCallbackId was called")
 	}
+
 	return (*ctx.cb)(c, d, event, ctx.f)
 }
 
-func (c *VirConnection) DomainEventRegister(dom VirDomain,
-	eventId int,
-	callback *DomainEventCallback,
-	opaque func()) int {
-	var callbackPtr unsafe.Pointer
-	context := &domainCallbackContext{
+func (c *VirConnection) DomainEventRegister(dom VirDomain, evID int, callback *DomainEventCallback, opaque func()) (cbID int, err error) {
+
+	var cbPtr unsafe.Pointer
+
+	ID := stDomCbCtx.Insert(&domainCallbackContext{
 		cb: callback,
 		f:  opaque,
-	}
-	goCallbackLock.Lock()
-	goCallBackId := nextGoCallbackId
-	nextGoCallbackId++
-	goCallbacks[goCallBackId] = context
-	goCallbackLock.Unlock()
+	})
 
-	switch eventId {
+	switch evID {
 	case VIR_DOMAIN_EVENT_ID_LIFECYCLE:
-		callbackPtr = unsafe.Pointer(C.domainEventLifecycleCallback_cgo)
-	case VIR_DOMAIN_EVENT_ID_REBOOT:
-	case VIR_DOMAIN_EVENT_ID_CONTROL_ERROR:
-		callbackPtr = unsafe.Pointer(C.domainEventGenericCallback_cgo)
+		cbPtr = unsafe.Pointer(C.domainEventLifecycleCallback_cgo)
+		evID = C.VIR_DOMAIN_EVENT_ID_LIFECYCLE
 	case VIR_DOMAIN_EVENT_ID_RTC_CHANGE:
-		callbackPtr = unsafe.Pointer(C.domainEventRTCChangeCallback_cgo)
+		cbPtr = unsafe.Pointer(C.domainEventRTCChangeCallback_cgo)
+		evID = C.VIR_DOMAIN_EVENT_ID_RTC_CHANGE
 	case VIR_DOMAIN_EVENT_ID_WATCHDOG:
-		callbackPtr = unsafe.Pointer(C.domainEventWatchdogCallback_cgo)
+		cbPtr = unsafe.Pointer(C.domainEventWatchdogCallback_cgo)
+		evID = C.VIR_DOMAIN_EVENT_ID_WATCHDOG
 	case VIR_DOMAIN_EVENT_ID_IO_ERROR:
-		callbackPtr = unsafe.Pointer(C.domainEventIOErrorCallback_cgo)
+		cbPtr = unsafe.Pointer(C.domainEventIOErrorCallback_cgo)
+		evID = C.VIR_DOMAIN_EVENT_ID_IO_ERROR
 	case VIR_DOMAIN_EVENT_ID_GRAPHICS:
-		callbackPtr = unsafe.Pointer(C.domainEventGraphicsCallback_cgo)
+		cbPtr = unsafe.Pointer(C.domainEventGraphicsCallback_cgo)
+		evID = C.VIR_DOMAIN_EVENT_ID_GRAPHICS
 	case VIR_DOMAIN_EVENT_ID_IO_ERROR_REASON:
-		callbackPtr = unsafe.Pointer(C.domainEventIOErrorReasonCallback_cgo)
+		cbPtr = unsafe.Pointer(C.domainEventIOErrorReasonCallback_cgo)
+		evID = C.VIR_DOMAIN_EVENT_ID_IO_ERROR_REASON
 	case VIR_DOMAIN_EVENT_ID_BLOCK_JOB:
-		// TODO Post 1.2.4, uncomment later
-		// case VIR_DOMAIN_EVENT_ID_BLOCK_JOB_2:
-		callbackPtr = unsafe.Pointer(C.domainEventBlockJobCallback_cgo)
+		cbPtr = unsafe.Pointer(C.domainEventBlockJobCallback_cgo)
+		evID = C.VIR_DOMAIN_EVENT_ID_BLOCK_JOB
 	case VIR_DOMAIN_EVENT_ID_DISK_CHANGE:
-		callbackPtr = unsafe.Pointer(C.domainEventDiskChangeCallback_cgo)
+		cbPtr = unsafe.Pointer(C.domainEventDiskChangeCallback_cgo)
+		evID = C.VIR_DOMAIN_EVENT_ID_DISK_CHANGE
 	case VIR_DOMAIN_EVENT_ID_TRAY_CHANGE:
-		callbackPtr = unsafe.Pointer(C.domainEventTrayChangeCallback_cgo)
-	case VIR_DOMAIN_EVENT_ID_PMWAKEUP:
-	case VIR_DOMAIN_EVENT_ID_PMSUSPEND:
-	case VIR_DOMAIN_EVENT_ID_PMSUSPEND_DISK:
-		callbackPtr = unsafe.Pointer(C.domainEventReasonCallback_cgo)
+		cbPtr = unsafe.Pointer(C.domainEventTrayChangeCallback_cgo)
+		evID = C.VIR_DOMAIN_EVENT_ID_TRAY_CHANGE
 	case VIR_DOMAIN_EVENT_ID_BALLOON_CHANGE:
-		callbackPtr = unsafe.Pointer(C.domainEventBalloonChangeCallback_cgo)
+		cbPtr = unsafe.Pointer(C.domainEventBalloonChangeCallback_cgo)
+		evID = C.VIR_DOMAIN_EVENT_ID_BALLOON_CHANGE
 	case VIR_DOMAIN_EVENT_ID_DEVICE_REMOVED:
-		callbackPtr = unsafe.Pointer(C.domainEventDeviceRemovedCallback_cgo)
+		cbPtr = unsafe.Pointer(C.domainEventDeviceRemovedCallback_cgo)
+		evID = C.VIR_DOMAIN_EVENT_ID_DEVICE_REMOVED
 	default:
+		cbPtr = nil
+		evID = -1
 	}
-	ret := C.virConnectDomainEventRegisterAny_cgo(c.ptr, dom.ptr, C.VIR_DOMAIN_EVENT_ID_LIFECYCLE,
-		C.virConnectDomainEventGenericCallback(callbackPtr),
-		C.int(goCallBackId))
-	if ret == -1 {
-		goCallbackLock.Lock()
-		delete(goCallbacks, goCallBackId)
-		goCallbackLock.Unlock()
-		return -1
+
+	if cbID = int(C.virConnectDomainEventRegisterAny_cgo(c.ptr, dom.ptr, C.int(evID), C.virConnectDomainEventGenericCallback(cbPtr), C.int(ID))); cbID == -1 {
+		stDomCbCtx.Erase(ID)
+		err = GetLastError()
 	}
-	return goCallBackId
+
+	return
 }
 
+/*
 func (c *VirConnection) DomainEventDeregister(callbackId int) int {
 	// Deregister the callback
 	return int(C.virConnectDomainEventDeregisterAny(c.ptr, C.int(callbackId)))
@@ -464,4 +491,232 @@ func EventRegisterDefaultImpl() int {
 
 func EventRunDefaultImpl() int {
 	return int(C.virEventRunDefaultImpl())
+}
+*/
+func (c *VirConnection) DomainEventDeregister(callbackId int) (err error) {
+
+	if rc := int(C.virConnectDomainEventDeregisterAny(c.ptr, C.int(callbackId))); rc == -1 {
+		err = GetLastError()
+	}
+
+	return
+}
+
+func EventRegisterDefaultImpl() (err error) {
+	if rc := int(C.virEventRegisterDefaultImpl()); rc == -1 {
+		err = GetLastError()
+	}
+
+	return
+}
+
+func EventRunDefaultImpl() (err error) {
+	if rc := int(C.virEventRunDefaultImpl()); rc == -1 {
+		err = GetLastError()
+	}
+
+	return
+}
+
+func (e *DomainLifecycleEvent) String() string {
+
+	var detail, event string
+	switch e.Event {
+
+	case VIR_DOMAIN_EVENT_DEFINED:
+		event = "defined"
+		switch e.Detail {
+
+		case VIR_DOMAIN_EVENT_DEFINED_ADDED:
+			detail = "added"
+
+		case VIR_DOMAIN_EVENT_DEFINED_UPDATED:
+			detail = "updated"
+
+		default:
+			detail = "unknown detail description"
+		}
+
+	case VIR_DOMAIN_EVENT_UNDEFINED:
+		event = "undefined"
+		switch e.Detail {
+
+		case VIR_DOMAIN_EVENT_UNDEFINED_REMOVED:
+			detail = "removed"
+
+		default:
+			detail = "unknown detail description"
+		}
+
+	case VIR_DOMAIN_EVENT_STARTED:
+		event = "started"
+		switch e.Detail {
+
+		case VIR_DOMAIN_EVENT_STARTED_BOOTED:
+			detail = "booted"
+
+		case VIR_DOMAIN_EVENT_STARTED_MIGRATED:
+			detail = "migrated"
+
+		case VIR_DOMAIN_EVENT_STARTED_RESTORED:
+			detail = "restored"
+
+		case VIR_DOMAIN_EVENT_STARTED_FROM_SNAPSHOT:
+			detail = "snapshot"
+
+		default:
+			detail = "unknown detail description"
+		}
+
+	case VIR_DOMAIN_EVENT_SUSPENDED:
+		event = "suspended"
+		switch e.Detail {
+
+		case VIR_DOMAIN_EVENT_SUSPENDED_PAUSED:
+			detail = "paused"
+
+		case VIR_DOMAIN_EVENT_SUSPENDED_MIGRATED:
+			detail = "migrated"
+
+		case VIR_DOMAIN_EVENT_SUSPENDED_IOERROR:
+			detail = "i/o error"
+
+		case VIR_DOMAIN_EVENT_SUSPENDED_WATCHDOG:
+			detail = "watchdog"
+
+		case VIR_DOMAIN_EVENT_SUSPENDED_RESTORED:
+			detail = "restored"
+
+		case VIR_DOMAIN_EVENT_SUSPENDED_FROM_SNAPSHOT:
+			detail = "snapshot"
+
+		default:
+			detail = "unknown detail description"
+		}
+
+	case VIR_DOMAIN_EVENT_RESUMED:
+		event = "resumed"
+		switch e.Detail {
+
+		case VIR_DOMAIN_EVENT_RESUMED_UNPAUSED:
+			detail = "unpaused"
+
+		case VIR_DOMAIN_EVENT_RESUMED_MIGRATED:
+			detail = "migrated"
+
+		case VIR_DOMAIN_EVENT_RESUMED_FROM_SNAPSHOT:
+			detail = "snapshot"
+
+		default:
+			detail = "unknown detail description"
+		}
+
+	case VIR_DOMAIN_EVENT_STOPPED:
+		event = "stopped"
+		switch e.Detail {
+
+		case VIR_DOMAIN_EVENT_STOPPED_SHUTDOWN:
+			detail = "shutdown"
+
+		case VIR_DOMAIN_EVENT_STOPPED_DESTROYED:
+			detail = "destroyed"
+
+		case VIR_DOMAIN_EVENT_STOPPED_CRASHED:
+			detail = "crashed"
+
+		case VIR_DOMAIN_EVENT_STOPPED_MIGRATED:
+			detail = "migrated"
+
+		case VIR_DOMAIN_EVENT_STOPPED_SAVED:
+			detail = "saved"
+
+		case VIR_DOMAIN_EVENT_STOPPED_FAILED:
+			detail = "failed"
+
+		case VIR_DOMAIN_EVENT_STOPPED_FROM_SNAPSHOT:
+			detail = "snapshot"
+
+		default:
+			detail = "unknown detail description"
+		}
+
+	case VIR_DOMAIN_EVENT_SHUTDOWN:
+		event = "shutdown"
+		switch e.Detail {
+
+		case VIR_DOMAIN_EVENT_SHUTDOWN_FINISHED:
+			detail = "finished"
+
+		default:
+			detail = "unknown detail description"
+		}
+
+	default:
+		event = "unknown event description"
+	}
+
+	return fmt.Sprintf("Domain event='%s' detail='%s'", event, detail)
+}
+
+func (e *DomainRTCChangeEvent) String() string {
+
+	return fmt.Sprintf("RTC change %d", e.Utcoffset)
+}
+
+func (e *DomainWatchdogEvent) String() string {
+
+	return fmt.Sprintf("Watchdog action='%d'", e.Action)
+}
+
+func (e *DomainIOErrorEvent) String() string {
+
+	return fmt.Sprintf("IO error path='%s' alias='%s' action='%d'", e.SrcPath, e.DevAlias, e.Action)
+}
+
+func (e *DomainGraphicsEvent) String() string {
+
+	var phase string
+	switch e.Phase {
+
+	case VIR_DOMAIN_EVENT_GRAPHICS_CONNECT:
+		phase = "connected"
+
+	case VIR_DOMAIN_EVENT_GRAPHICS_INITIALIZE:
+		phase = "initialized"
+
+	case VIR_DOMAIN_EVENT_GRAPHICS_DISCONNECT:
+		phase = "disconnected"
+	}
+
+	return fmt.Sprintf("Graphics phase='%s'", phase)
+}
+
+func (e *DomainIOErrorReasonEvent) String() string {
+
+	return fmt.Sprintf("IO error path='%s' alias='%s' action='%d' reason='%s'", e.SrcPath, e.DevAlias, e.Action, e.Reason)
+}
+
+func (e *DomainBlockJobEvent) String() string {
+
+	return fmt.Sprintf("Block job disk='%s' status='%d' type='%d'", e.Disk, e.Status, e.Type)
+}
+
+func (e *DomainDiskChangeEvent) String() string {
+
+	return fmt.Sprintf("Disk change old='%s' new='%s' alias='%s' reason='%d'", e.OldSrcPath, e.NewSrcPath, e.DevAlias, e.Reason)
+}
+
+func (e *DomainTrayChangeEvent) String() string {
+
+	return fmt.Sprintf("Tray change dev='%s' reason='%d'", e.DevAlias, e.Reason)
+}
+
+func (e *DomainBalloonChangeEvent) String() string {
+
+	return fmt.Sprintf("Ballon change '%d'", e.Actual)
+}
+
+func (e *DomainDeviceRemovedEvent) String() string {
+
+	return fmt.Sprintf("Device '%s' removed ", e.DevAlias)
 }
